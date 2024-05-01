743023b47d0185f4fd2022850c39e9749dc6030f
fac1L <galtahgui.ni@gmail.com>
2024-05-01 00:33:50 -0300

    Notificaciones
    -componente de notificaciones creado e implementado

--- src/config/theme.config.tsx
+++ src/config/theme.config.tsx
@@ -9,6 +9,11 @@ enum themePalette {
     BG = '#053545',
     CRUNCHY = '#f47521',
     FONT_GLOBAL = 'Roboto',
+    // alertas (fijate si lo usas)
+    // FONT_COLOR = "#000000",
+    // BG_ALERT = "#ffffff",
+    // ERROR_MAIN = "#f44336",
+    // BG_ERROR_MAIN = "rgba(244,67,54,0.1)"
 }
 
 const theme = createTheme({
@@ -35,6 +40,28 @@ const theme = createTheme({
                     textTransform: 'none',
                 },
             },
+        },
+        MuiAlert: {
+            defaultProps: {
+                variant: "filled",
+                // style: {
+                //     borderRadius: "0.8em",
+                //     fontSize: "1em"
+                // },
+            },
+            // styleOverrides: {
+            //     root: {
+            //         backgroundColor: themePalette.BG_ALERT,
+            //         color: themePalette.FONT_COLOR,
+            //         border: `5px solid ${themePalette.ERROR_MAIN}`,
+            //     }
+            // }
+            // styleOverrides: {
+            //     standardError: {
+            //         border: "1px solid ${themePalette.ERROR_MAIN}",
+            //         background: themePalette.BG_ERROR_MAIN,
+            //     },
+            // },
         },
     },
 });
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Button, Container } from "@mui/material";
+import { useNotification } from "../context/notification.context";
 
 export const HomePage: React.FC<{}> = () => {
+    const { getError, getSuccess } = useNotification()
+    const handleClick = () => {
+        getError("Hola, estas en el home")
+    }
+
     return (
         <Container sx={{ mt: 9 }} maxWidth="xl">
-            <Button variant='contained'>SOY EL HOME</Button>
+            <Button onClick={handleClick} variant='contained'>SOY EL HOME</Button>
         </Container>
     );
 }
