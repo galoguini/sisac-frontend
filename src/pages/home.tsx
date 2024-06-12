600eb47c62269e9ed6123dafe866009213cfc4d2
fac1L <galtahgui.ni@gmail.com>
2024-06-12 19:20:38 -0300

    .
    .

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Button, Container, Paper, Typography } from "@mui/material";
-import { useNotification } from "../context/notification.context";
+import { Container, Typography } from "@mui/material";
+// import { useNotification } from "../context/notification.context";
 
 export const HomePage: React.FC<{}> = () => {
-    const { getSuccess, getError, getInfo, getWarning } = useNotification();
-    const handleClick = () => {
-        getSuccess('Mensaje de éxito');
-        // getError('Mensaje de error');
-        // getInfo('Mensaje de información');
-        // getWarning('Mensaje de advertencia');
-    }
+    // const { getSuccess, getError, getInfo, getWarning } = useNotification();
+    // const handleClick = () => {
+    //     getSuccess('Mensaje de éxito');
+    //     getError('Mensaje de error');
+    //     getInfo('Mensaje de información');
+    //     getWarning('Mensaje de advertencia');
+    // }
     return (
         <Container sx={{ mt: 9 }} maxWidth="xs">
-            {/* <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
-            </Paper> */}
             <Typography variant="h4" align="center">Sistema de Administración Contable</Typography>
         </Container>
     );
