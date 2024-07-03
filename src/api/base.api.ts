cc363bade2f1465224d980d1e713c8f9e621f2eb
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-07-03 18:14:55 -0300

    base url agregado

--- src/api/base.api.ts
+++ src/api/base.api.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
 
-// const BASE_URL = "https://galoguini.pythonanywhere.com/api/v1/"
-const BASE_URL = "http://127.0.0.1:8000/api/v1/"
+const BASE_URL = "https://web-production-d54a.up.railway.app/api/v1/"
 
 export const apiURL = axios.create({
     baseURL: BASE_URL
