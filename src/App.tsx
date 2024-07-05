54d054ac49091c114e38b84e79b2d204554f802d
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-07-05 02:27:35 -0300

    corrección del uso de notificaciones

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { AppRouter } from './Router';
-import { NotificationProvider, useNotification } from './context/notification.context';
+import { NotificationProvider } from './context/notification.context';
 import ActivityMonitor from './common/ActivityMonitor';
 import { checkAuth } from './api/usuarios';
 
 function App() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
-    const { getInfo } = useNotification();
 
     useEffect(() => {
         const authenticate = async () => {
@@ -22,8 +21,8 @@ function App() {
     }, []);
 
     if (isLoading) {
-        getInfo('Obteniendo información de sesión...');
-        return <div>Cargando...</div>;
+        console.log('Obteniendo información de sesión...');
+        return <div>Obteniendo información de sesión...</div>;
     }
 
     return (
