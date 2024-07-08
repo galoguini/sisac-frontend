1eb8df94b1076b2107388d013c9f1304e6833c7c
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-07-07 22:30:45 -0300

    configuración para el uso de variables de entorno

--- src/api/base.api.ts
+++ src/api/base.api.ts
@@ -1,7 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = "https://web-production-d54a.up.railway.app/api/v1/"
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+console.log('BASE_URL', BASE_URL);
 
 export const apiURL = axios.create({
     baseURL: BASE_URL
-})
+});
